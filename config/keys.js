8d43bc3fecfbf9dc19a22062dce8581869155338
Gabrych Mariusz <gabrycm@omnis.netia.org>
2019-11-21 11:29:41 +0100

    start

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://marian:db123@starting-cluster-quwhy.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://marian:db123@mern-cluster-xrjzd.mongodb.net/test?retryWrites=true&w=majority'
+    // mongoURI: 'mongodb+srv://marian:db123@starting-cluster-quwhy.gcp.mongodb.net/test?retryWrites=true&w=majority'
 };
